2d8d3221a19431fa99cfb270681218a13ae9e104
brandonshelby <145580496+brandonshelby@users.noreply.github.com>
2023-09-27 07:39:22 -0600

    tambien esta padre

---
+++  arreglos/src/arreglos/Arreglos.java
@@ -0,0 +1,14 @@
+
+package arreglos;
+import java.util.Scanner{
+public class Arreglos {
+ public static void main(String[] args) {
+ Scanner sn=new Scanner(system.in);
+ num
+     
+     
+ }
+ }
+ }
+    
+
